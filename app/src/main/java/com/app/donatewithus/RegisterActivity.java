3477de1606d8c72a1235a6df4d1a452c989149c5
kavindu <kavindu@echonlabs.com>
2022-11-28 00:31:45 +0530

    Register Activity modified

--- app/src/main/java/com/app/donatewithus/RegisterActivity.java
+++ app/src/main/java/com/app/donatewithus/RegisterActivity.java
@@ -2,13 +2,38 @@ package com.app.donatewithus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button userRegBtn;
+    Button clubRegBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        userRegBtn = findViewById(R.id.register_btn_userRegButton);
+        clubRegBtn = findViewById(R.id.register_btn_clubRegButton);
+
+        userRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, RegisterActivityUser.class);
+                startActivity(intent);
+            }
+        });
+
+        clubRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, RegisterActivityClub.class);
+                startActivity(intent);
+            }
+        });
     }
 }
