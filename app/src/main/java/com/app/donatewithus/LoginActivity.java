c11ede39ebe34f3f907752d1c21b1391647de998
kavindu <kavindu@echonlabs.com>
2022-11-28 00:25:03 +0530

    Project Structure Updated

--- app/src/main/java/com/app/donatewithus/LoginActivity.java
+++ app/src/main/java/com/app/donatewithus/LoginActivity.java
@@ -2,13 +2,36 @@ package com.app.donatewithus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginBtn;
+    TextView registerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.login_btn_loginButton);
+        registerTextView = findViewById(R.id.loginPageQuestion);
+
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
+
 }
